07216c1abab5ff0396d7a5ef5cf328cc4c722745
NEMURENAIYORU <lhslhs301@gmail.com>
2024-09-08 20:59:53 +0900

    init

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+class Department {
+  // private readonly id: string;
+  // name: string;
+  private employees: string[] = [];
+
+  constructor(private readonly id: string, public name: string) {
+  
+  }
+
+  describe(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+const accounting = new Department('D1', 'Accounting');
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+
+accounting.describe();
+accounting.printEmployeeInformation();
