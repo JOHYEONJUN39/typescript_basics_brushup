75c98dff0d65cd808a221d08edd95fde9114dd67
NEMURENAIYORU <lhslhs301@gmail.com>
2024-09-28 21:13:13 +0900

    2024.9.28

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 class Department {
+  static fiscalYear = 2024;
   // private readonly id: string;
   // name: string;
-  private employees: string[] = [];
+  protected employees: string[] = [];
+
+  static createEmployee(name: string) {
+    return {name: name};
+  }
 
   constructor(private readonly id: string, public name: string) {
-  
+    console.log(Department.fiscalYear)
   }
 
   describe(this: Department) {
@@ -21,10 +26,79 @@ class Department {
   }
 }
 
-const accounting = new Department('D1', 'Accounting');
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, 'IT');
+    this.admins = admins;
+  }
+}
 
-accounting.addEmployee('Max');
-accounting.addEmployee('Manu');
+class AccountingDepartment extends Department {
+  private lastReport: string;
 
-accounting.describe();
-accounting.printEmployeeInformation();
+  // 캡슐화 구현 (데이터나 관리를 은닉 및 보호)
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error('No report found.');
+  }
+
+  set mostRecentReport(value: string) {
+    if(!value) {
+      throw new Error('Please pass in a valid value!');
+    }
+    this.addReport(value);
+  }
+
+  constructor(id: string, private reports: string[]) {
+    super(id, 'Accounting');
+    this.lastReport = reports[0];
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+    this.lastReport = text;
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+
+  addEmployee(name: string): void {
+      if (name === 'Max') {
+        return;
+      }
+      this.employees.push(name);
+  }
+}
+
+const employee1 = Department.createEmployee('Max');
+console.log(employee1, Department.fiscalYear);
+
+const it = new ITDepartment('d1', ['Max']);
+Math
+
+const account = new AccountingDepartment('d2', []);
+
+// account.addEmployee('Max');
+// account.addEmployee('Manu');
+// account.printEmployeeInformation();
+
+account.mostRecentReport = 'Year End Report';
+// account.addReport('Something went wrong...');
+// account.addReport('Something went right...');
+// account.addReport('Something went left...');
+console.log(account.mostRecentReport);
+// account.printReports();
+// account.describe();
+
+// it.addEmployee('Max');
+// it.addEmployee('Manu');
+
+// it.describe();
+// it.printEmployeeInformation();
+
+// console.log(it);
+// console.log(account);
